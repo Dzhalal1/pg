b8bbedd5a6a1b042546807fbaf56604246565553
Dzhalal <12345@mail.ru>
2020-11-19 11:16:34 +0300

    commit login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,48 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import {createRouter, createWebHistory} from '@ionic/vue-router';
+import {RouteRecordRaw} from 'vue-router';
 import Tabs from '../views/Tabs.vue'
-
+import Login from '../views/Login.vue'
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/tabs/tab1'
-  },
-  {
-    path: '/tabs/',
-    component: Tabs,
-    children: [
-      {
-        path: '',
-        redirect: 'tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      },
-      {
-        path: 'subjects',
-        component: () => import('@/views/Subjects.vue')
-      }
-    ]
-  }
+    {
+        path: '/',
+        redirect: '/tabs/tab1'
+    },
+    {
+        path: '/tabs/',
+        component: Tabs,
+        children: [
+            {
+                path: '',
+                redirect: 'tab1'
+            },
+            {
+                path: 'tab1',
+                component: () => import('@/views/Tab1.vue')
+            },
+            {
+                path: 'tab2',
+                component: () => import('@/views/Tab2.vue')
+            },
+            {
+                path: 'tab3',
+                component: () => import('@/views/Tab3.vue')
+            },
+            {
+                path: 'subjects',
+                component: () => import('@/views/Subjects.vue')
+            },
+
+        ]
+    },
+    {
+        path: '/login',
+        component: Login
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,99 @@
+<template>
+    <ion-page>
+        <ion-grid>
+            <ion-row>
+                <ion-col>
+                    <ion-icon name="person-outline"></ion-icon>
+                    <ion-input placeholder="Username"></ion-input>
+                </ion-col>
+            </ion-row>
+            <ion-row>
+                <ion-col>
+                    <ion-icon name="person-outline"></ion-icon>
+                    <ion-input placeholder="Password"></ion-input>
+                </ion-col>
+            </ion-row>
+            <ion-row>
+                <ion-col>
+                    <ion-button expand="full" fill="clear">LOGIN</ion-button>
+                </ion-col>
+            </ion-row>
+
+        </ion-grid>
+    </ion-page>
+</template>
+
+<script>
+    import {
+        IonPage,
+        IonInput,
+        IonButton,
+        IonLabel,
+        IonItem,
+        IonIcon,
+        IonCol,
+        IonRow,
+        IonGrid,
+        // IonHeader,
+        // IonToolbar,
+        // IonTitle,
+        // IonContent
+    } from '@ionic/vue';
+    import ExploreContainer from '@/components/ExploreContainer.vue';
+    import {lockClosed, personOutline} from 'ionicons/icons';
+
+    export default {
+        name: 'Login',
+        components: {
+            // ExploreContainer,
+            IonInput,
+            IonButton,
+            IonCol,
+            IonRow,
+            IonGrid,
+            // IonLabel,
+            // IonItem,
+            IonIcon,
+            // IonHeader,
+            // IonToolbar,
+            // IonTitle,
+            // IonContent,
+            IonPage
+        },
+        setup() {
+            return {
+                lockClosed,
+                personOutline
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ion-input {
+        background-color: #cacaca !important;
+        border-radius: 30px;
+        height: 80%;
+        width: 300px;
+        padding-left: 1em !important;
+        color: black;
+        font-family: "TRY Clother Black";
+        /*font-family: "Jost SemiBold";*/
+    }
+
+    ion-button {
+        border-radius: 100px;
+        background-color: lightseagreen;
+        color: white;
+        font-family: "TRY Clother Black";
+        /*display: flex;*/
+        /*width: 50%;/*/
+
+
+
+    }
+
+    ion-item {
+        background-color: red !important;
+    }
+</style>
