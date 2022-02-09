de47260af7a5755cea0ecedb60c786a82b0beb75
User <artela1992@gmail.com>
2022-02-09 14:42:53 +0300

    поменял расписание и шрифт

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,7 +28,7 @@
             </ion-col>
           </ion-row>
         </a>
-        <a href="https://kgsxa.ru/images/files/raspisanie_2021-2022_1/www/bg.htm">
+        <a href="https://kgsxa.ru/studentam/raspisanie-zanyati">
           <ion-row class="calendar ion-align-items-center ion-activatable ripple-parent">
             <ion-ripple-effect></ion-ripple-effect>
             <ion-col>
